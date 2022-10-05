f3e430aa4e43a58fc398d1c088479f36b5f8dc89
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-10-05 09:45:06 +0530

    pagy

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 gem 'devise'
 
 gem 'social-share-button'
+
+gem "pagy"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
+    @pagy, @posts = pagy(Post.order(created_at: :desc), items: 1)
+
     @user = User.all
     @my_account = current_user
+
   end
 end
